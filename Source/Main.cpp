ab83df4b5af67e371d6ed5272418d673e10bfd08
gvne <guillaume.vincke@gmail.com>
2020-06-01 14:59:31 +0200

    Implement basic I/O functions

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,21 +1,44 @@
-/*
-  ==============================================================================
-
-    This file was auto-generated!
-
-    It contains the basic startup code for a JUCE application.
-
-  ==============================================================================
-*/
-
 #include <JuceHeader.h>
+#include <cxxopts.hpp>
 
-//==============================================================================
-int main (int argc, char* argv[])
-{
+#include "io.h"
+#include "utils.h"
 
-    // ..your code goes here!
-
-
+int main (int argc, char* argv[]) {
+  cxxopts::Options options("spleet", "Split audio");
+  options.add_options()
+    ("i,input", "Input file path", cxxopts::value<std::string>())
+    ("o,output", "Output directory path", cxxopts::value<std::string>()->default_value("./spleet"))
+    ("c,count", "Number of output files", cxxopts::value<int>()->default_value("2"))
+    ("h,help", "Print usage")
+    ;
+  
+  auto result = options.parse(argc, argv);
+  if (result.count("help")) {
+    std::cout << options.help() << std::endl;
     return 0;
+  }
+  
+  // TODO: validate the parameters (output count) and initialize spleeter
+  
+  
+  double input_sampling_rate = 0;
+  const double process_sampling_rate = 44100;
+  Eigen::MatrixXf data;
+  std::error_code err;
+  Read(result["input"].as<std::string>(), &data, &input_sampling_rate, err);
+  if (err) {
+    std::cout << "Couldn't read source file" << std::endl;
+    return 0;
+  }
+  
+  auto waveform = Resample(Stereo(data), input_sampling_rate, process_sampling_rate);
+  // TODO: process
+  
+  Write(waveform, process_sampling_rate, result["output"].as<std::string>(), err);
+  if (err) {
+    std::cout << "Couldn't export at " << std::endl;
+  }
+
+  return 0;
 }
---
+++ Source/utils.cpp
@@ -0,0 +1,51 @@
+/*
+  ==============================================================================
+
+    utils.cpp
+    Created: 1 Jun 2020 2:34:45pm
+    Author:  gvne
+
+  ==============================================================================
+*/
+
+#include "utils.h"
+#include <Eigen/Core>
+#include <JuceHeader.h>
+
+Eigen::MatrixXf Resample(const Eigen::MatrixXf &source, double original,
+                         double desired) {
+  if (desired == original) {
+    return source;
+  }
+
+  LagrangeInterpolator interpolator;
+  auto speed_ratio = desired / original;
+
+  Eigen::MatrixXf output(
+      source.rows(), static_cast<int>(floor(source.cols() * speed_ratio + 1)));
+
+  Eigen::VectorXf input_channel(source.cols());
+  Eigen::VectorXf output_channel(output.cols());
+  for (auto channel_idx = 0; channel_idx < source.rows(); channel_idx++) {
+    interpolator.reset();
+    interpolator.process(speed_ratio, input_channel.data(),
+                         output_channel.data(), output_channel.cols());
+    output.row(channel_idx) = output_channel;
+  }
+
+  return output;
+}
+
+Eigen::MatrixXf Stereo(const Eigen::MatrixXf &data) {
+  if (data.rows() == 2) {
+    return data;
+  }
+  // make the mean over rows
+  auto mean = data.array().rowwise().sum() / data.rows();
+
+  // duplicate it twice to get stereo
+  Eigen::MatrixXf output(2, data.cols());
+  output.row(0) = mean;
+  output.row(1) = mean;
+  return output;
+}
